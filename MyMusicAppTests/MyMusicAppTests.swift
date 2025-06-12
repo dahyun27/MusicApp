8742b49d2f61bf007febe88c4c8d527485bdc473
Ha Da Hyun <https://github.com/dahyun27>
2025-06-12 20:08:00 +0900

    Initial Commit

---
+++ MyMusicAppTests/MyMusicAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicAppTests.swift
+//  MyMusicAppTests
+//
+//  Created by 하다현 on 6/12/25.
+//
+
+import Testing
+@testable import MyMusicApp
+
+struct MyMusicAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
