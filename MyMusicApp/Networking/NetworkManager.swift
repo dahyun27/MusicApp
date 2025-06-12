390174935d4de0b81e3090da5d22dd0aabfde38e
Ha Da Hyun <https://github.com/dahyun27>
2025-06-12 20:32:19 +0900

    Add NetworkManager, Music

--- MyMusicApp/Networking/NetworkManager.swift
+++ MyMusicApp/Networking/NetworkManager.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+enum NetworkError {
+    case networkingError
+    case dataError
+    case parseError
+}
+
+//MARK: - Networking (서버와 통신하는) 클래스 모델
+final class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+    
+}
